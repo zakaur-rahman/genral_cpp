7ac73802a60ffe9317167ff05f7f325c33c9e123
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-03-29 22:39:56 +0530

    trianglePatternUsingWhileLoop.cpp

---
+++ trianglePatternUsingWhileLoop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    
+    int a= 4;
+    int i=1;
+while(i<=a){
+    
+    int j=1;
+    while(j<=i){
+       cout<<"*"<<" ";
+       j+=1;
+    
+    }cout<<endl;
+    i+=1;
+}
+
+    return 0;
+}
