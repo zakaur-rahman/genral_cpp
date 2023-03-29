b98a6d9e1208ac6aba350a40560670e3897af23c
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-03-29 22:24:29 +0530

    SequentialNumberMatrix.cpp

---
+++ sequentialNumberMatrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    
+    int a= 3, b=0;
+    for(int i= 1; i<=a; i++){
+        for(int j=1;j<=a;j++){
+            b+=1;
+           cout<<b<<" "; 
+        }cout<<endl;
+        
+    }
+
+    return 0;
+}
