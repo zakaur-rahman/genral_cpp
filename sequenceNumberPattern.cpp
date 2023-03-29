0c4b082f2001fe520d9709855b5dc877ff795398
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-03-30 00:21:18 +0530

    sequenceNumberPattern.cpp

---
+++ sequenceNumberPattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    
+    int a= 4;
+    int i=1;
+    int b=0;
+while(i<=a){
+    
+    int j=1;
+    while(j<=i){
+        b+=1;
+       cout<<b<<" ";
+       j+=1;
+    
+    }cout<<endl;
+    i+=1;
+}
+
+    return 0;
+}
