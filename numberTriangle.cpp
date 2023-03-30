fc341202972cdc6b9eb70e843fae2fd31cac65a8
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-03-30 12:52:31 +0530

    variable removed

--- numberTriangle.cpp
+++ numberTriangle.cpp
@@ -16,3 +16,23 @@ int main() {
 
     return 0;
 }
+
+//without taking extra variable
+/*
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n=4; int row=1;
+    while(row<=n){
+        int col=1;
+        while(col<=row){
+            cout<<row-col+1<<" ";
+            col+=1;
+        }cout<<endl;
+        row+=1;
+    }
+
+    return 0;
+}
+*/
