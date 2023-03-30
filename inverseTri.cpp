20b82d1c7db255370384157a4f0ee0f45a8efe42
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-03-30 16:26:21 +0530

    inverseTriangle

---
+++ inverseTri.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n=5; 
+    
+    
+    int row=1; 
+    while(row<=n){
+        int col=1;
+        while(col<=row-1){
+            cout<<" ";
+            col+=1;
+        }
+        
+        while(col<=n){
+            cout<<"*";
+            col+=1;
+        }
+        cout<<endl;
+        row+=1;
+    }
+    return 0;
+}
+
+//  output
+/*  
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
