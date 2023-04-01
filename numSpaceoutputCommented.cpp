970068ea2d09a42bda6253f04518a9a55c91dea2
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-04-01 15:50:48 +0530

    numSpaceoutputCommented

---
+++ numSpaceoutputCommented.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int row=1, n=4 , b=0;
+   while(row<=n){
+       int col=n-row;
+       while(col){
+           cout<<" ";
+           col-=1;
+       }
+       int val=1;
+       while(val<=row){
+           b+=1;
+           cout<<b;
+           val+=1;
+       }
+       cout<<endl;
+       row+=1;
+   }
+   
+}
+
+/*
+   1
+  23
+ 456
+78910
+*/
