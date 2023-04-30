4874da33a79925a13131cdb1e4975d3ad90c273c
Zakaur Rahman <zakaurrahman67@gmail.com>
2023-04-30 16:08:35 +0530

    formula for mid is changed

--- binarSearch.cpp
+++ binarSearch.cpp
@@ -6,7 +6,7 @@ int binarySearch(int arr[], int size, int key){
     int start = 0;
     int end = size - 1;
     while(start <= end){
-        int mid = (start + end)/2;
+        int mid = start + (end - start)/2;
         
         if(key==arr[mid]){
             return mid;
@@ -16,7 +16,7 @@ int binarySearch(int arr[], int size, int key){
         }
         else{
             end = mid - 1;
-        }mid = (start + end)/2;
+        }mid = start + (end - start)/2;
         
     }return -1;
 }
